2d128059e2d73971e3d0f3140efc5a8d2214d01f
erizheng <114521575+erizheng@users.noreply.github.com>
2022-11-10 12:22:24 -0800

    lab week 7

--- grade.sh
+++ grade.sh
@@ -1,7 +1,21 @@
 # Create your grading script here
-
 set -e
+
+CPATH = .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar*
 
 rm -rf student-submission
 git clone $1 student-submission
+
+cp TestListExamples.java student-submission
+
+cd student-submission
+
+
+if [[-e ListExamples.java]] 
+    then echo "Contains file"
+    else exit
+fi
+
+javac -cp $CPATH.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
